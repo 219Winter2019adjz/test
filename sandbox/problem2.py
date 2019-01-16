88394da8ecda54c6508573e75dbdc004db445997
Zach Harris <zharris505@gmail.com>
2019-01-15 22:14:28 -0800

    Adding rough draft of problem 2

--- sandbox/problem2.py
+++ sandbox/problem2.py
@@ -1,4 +1,135 @@
-from sklearn.feature_extraction import text
-stop_words = text.ENGLISH_STOP_WORDS
-print(stop_words)
-print(len(stop_words))
+########################################################################################################################
+# Fetching 20NewsGroups dataset
+from sklearn.datasets import fetch_20newsgroups
+# Refer to the offcial document of scikit-learn for detailed usages:
+# http://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_20newsgroups.html
+categories = ['comp.graphics', 'comp.sys.mac.hardware']
+twenty_train = fetch_20newsgroups(subset='train', # choose which subset of the dataset to use; can be 'train', 'test', 'all'
+                                  categories=categories, # choose the categories to load; if is `None`, load all categories
+                                  shuffle=True,
+                                  random_state=42, # set the seed of random number generator when shuffling to make the outcome repeatable across different runs
+                                  # remove=['headers'],
+                                  )
+twenty_test = fetch_20newsgroups(subset='test', categories=categories, shuffle=True, random_state=42)
+
+########################################################################################################################
+# Perform lemmatization on dataset
+
+# The lemmatizer is actually pretty complicated, it needs Parts of Speech (POS) tags
+import nltk
+from nltk import pos_tag
+# nltk.download('punkt')#, if you need "tokenizers/punkt/english.pickle", choose it
+# nltk.download('averaged_perceptron_tagger')
+
+def penn2morphy(penntag):
+    """ Converts Penn Treebank tags to WordNet. """
+    morphy_tag = {'NN': 'n', 'JJ': 'a',
+                  'VB': 'v', 'RB': 'r'}
+    try:
+        return morphy_tag[penntag[:2]]
+    except:
+        return 'n'
+
+
+# def lemmatize_sent(list_word, wnl):
+#     # Text input is string, returns array of lowercased strings(words).
+#     return [wnl.lemmatize(word.lower(), pos=penn2morphy(tag))
+#             for word, tag in pos_tag(list_word)]
+
+
+wnl = nltk.wordnet.WordNetLemmatizer()
+def lemmatize_training(text):
+    # Text input is string, returns array of lowercased strings(words).
+    return [wnl.lemmatize(word.lower(), pos=penn2morphy(tag))
+            for word, tag in pos_tag(nltk.word_tokenize(text))]
+
+
+# TODO: should this filter out the following numbers too? "4-5" "c650"
+def filter_numbers(text_array):
+    # Filter out any numbers found in the array of strings
+    output = []
+    for s in text_array:
+        if not s.isdigit():
+            # if not a digit...
+            try:
+                # if a float, filter out
+                float(s)
+            except ValueError:
+                # if not a float, add to output
+                output.append(s)
+        else:
+            # if a digit, filter out
+            pass
+    return output
+
+
+def array_to_string(text_array, delimeter=""):
+    # Converts an array back into a string of words using the provided delimeter to add between each word
+    output = ""
+    for s in text_array:
+        output = output + delimeter + s
+    return output
+
+
+def lemmatize_and_filter(documents):
+    # Performs lemmatization, and number filtering on the given documents
+    lemmatized_data = []
+    for i in documents:
+        # lemmatize the document:
+        training_tagged = pos_tag(nltk.word_tokenize(i))
+        lemmatized_array = lemmatize_training(i)
+
+        # remove numbers from document:
+        filtered_array = filter_numbers(lemmatized_array)
+
+        # reassemble back to string:
+        lemmatized_string = array_to_string(filtered_array, ' ')
+
+        # add to final data list
+        # print(lemmatized_string)
+        lemmatized_data.append(lemmatized_string)
+
+    return lemmatized_data
+
+
+# print(lemmatized_data[0])
+lemmatized_training = lemmatize_and_filter(twenty_train.data)
+lemmatized_testing = lemmatize_and_filter(twenty_test.data)
+
+########################################################################################################################
+# Push lemmatized documents through CountVectorizer
+
+# count_vect = CountVectorizer(min_df=3)
+from sklearn.feature_extraction.text import CountVectorizer
+
+# do for training
+count_vect = CountVectorizer(min_df=3, stop_words='english')
+X_train_counts = count_vect.fit_transform(lemmatized_training)
+
+# do for testing
+X_test_counts = count_vect.transform(lemmatized_testing)
+
+########################################################################################################################
+# Report shapes of TF-IDF matrices
+
+from sklearn.feature_extraction.text import TfidfTransformer
+tfidf_transformer = TfidfTransformer()
+
+# do for training
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+print(X_train_tfidf.shape)
+print('-' * 20)
+print(X_train_counts.toarray()[:30, :5])
+print('-' * 20)
+print(X_train_tfidf.toarray()[:30, :5])
+
+# do for testing
+X_test_tfidf = tfidf_transformer.transform(X_test_counts)
+
+print(X_test_tfidf.shape)
+print('-' * 20)
+print(X_test_counts.toarray()[:30, :5])
+print('-' * 20)
+print(X_test_tfidf.toarray()[:30, :5])
+
