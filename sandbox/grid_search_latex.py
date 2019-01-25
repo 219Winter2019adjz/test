65ed6a87f3caed7faf8742d23dcfcd32997ad266
Zach Harris <zharris505@gmail.com>
2019-01-24 19:10:44 -0800

    FINAL project submission

--- sandbox/grid_search_latex.py
+++ sandbox/grid_search_latex.py
@@ -6,15 +6,23 @@ pd.set_option('display.max_colwidth', 20)
 sorted = results.sort_values(by=['mean_test_score'], ascending=False)
 # print(sorted)
 
-print(sorted.iloc[0, 10])
-exit(0)
+print(sorted.iloc[18, 10])
 
-cropped1 = sorted.iloc[0:20, [3] + [4] + [5] + [-1] + [-2]]
-cropped1.to_latex("grid_search_results_latex1.txt")
-print(cropped1)
-
-pd.set_option('display.max_colwidth', 16)
-cropped2 = sorted.iloc[0:20, 6:10]# + [-1] + [-2]]
-# cropped2 = sorted.iloc[0:20, [6] + [6] + [7] + [8] + [9] + [10]]# + [-1] + [-2]]
-cropped2.to_latex("grid_search_results_latex2.txt", index=False)
-print(cropped2)
+# results = pd.read_csv("..\grid_search_results_temp.csv")
+# pd.set_option('display.max_colwidth', 20)
+#
+# sorted = results.sort_values(by=['mean_test_score'], ascending=False)
+# # print(sorted)
+#
+# print(sorted.iloc[0, 10])
+# # exit(0)
+#
+# cropped1 = sorted.iloc[0:20, [3] + [4] + [5] + [-1] + [-2]]
+# cropped1.to_latex("grid_search_results_latex1_temp.txt")
+# print(cropped1)
+#
+# pd.set_option('display.max_colwidth', 16)
+# cropped2 = sorted.iloc[0:20, 6:10]# + [-1] + [-2]]
+# # cropped2 = sorted.iloc[0:20, [6] + [6] + [7] + [8] + [9] + [10]]# + [-1] + [-2]]
+# cropped2.to_latex("grid_search_results_latex2_temp.txt", index=False)
+# print(cropped2)
